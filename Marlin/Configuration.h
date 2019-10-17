df20cef466391ffa192246d57d1547a03e762ec9
Kokorone <32761171+Kokorone@users.noreply.github.com>
2019-10-17 16:06:47 -0700

    Changing bed offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1066,7 +1066,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS -20
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE + 15
 #define Y_MAX_POS Y_BED_SIZE
