e39307296a09e47b17a39532e6f9bd84ae1124ef
Kokorone <32761171+Kokorone@users.noreply.github.com>
2019-12-02 10:02:19 -0800

    Removing incompatible s curve

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -776,7 +776,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define CLASSIC_JERK
+//#define CLASSIC_JERK
 #if ENABLED(CLASSIC_JERK)
   #define DEFAULT_XJERK 10.0
   #define DEFAULT_YJERK 10.0
@@ -809,7 +809,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-#define S_CURVE_ACCELERATION
+//#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
