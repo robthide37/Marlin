d3fe6e7dfcb0db3e1e31137847da16e85b4a4351
Kokorone <gabehahr@gmail.com>
2020-02-17 20:25:19 -0800

    upping Vref

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2086,7 +2086,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      680
+    #define E0_CURRENT      730
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
